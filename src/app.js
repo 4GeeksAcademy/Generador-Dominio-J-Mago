5fa0a5917513579738f9e68fa596444898082968
Jfmm1983 <123413274+Jfmm1983@users.noreply.github.com>
2025-01-22 22:12:40 +0000

    fix/app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
+  let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dom =[".com"]
+
+for (prop of pronoun){
+
+  for (prop1 of adj){
+
+    for (prop2 of noun){
+
+      console.log(prop+prop1+prop2+dom)
+    }
+
+  }
+ 
+}
+  
   console.log("Hello Rigo from the console!");
 };
