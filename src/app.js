2b9da67c0e2a68511f84079dfc2db960a840401d
Jfmm1983 <123413274+Jfmm1983@users.noreply.github.com>
2025-01-24 00:30:11 +0000

    fix/app.js

--- src/app.js
+++ src/app.js
@@ -7,23 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
 
+
   let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let dom =[".com"]
-
-for (prop of pronoun){
-
-  for (prop1 of adj){
-
-    for (prop2 of noun){
-
-      console.log(prop+prop1+prop2+dom)
-    }
-
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  
+  let dom =[".com", ".net", ".ve", ".gov", ".uy"]
+  
+  for (let element1 of pronoun){
+  
+    for (let element2 of adj){
+  
+      for (let element3 of noun){
+  
+        console.log(element1 + element2 + element3 + dom [generarNumeroAleatorio(0,5)])
+      }
+  
+    }1
+   
   }
- 
-}
+  
+  function generarNumeroAleatorio(min, max){
+  
+    return Math.floor(Math.random()*(max-min))
+  
+  }
+  
   
   console.log("Hello Rigo from the console!");
 };
